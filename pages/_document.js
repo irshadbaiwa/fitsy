7b9a586aa4dd95c8218c3da8d527415c754a6f1a
Abubakar Baiwa <irshaadbaiwa@gmail.com>
2022-06-22 10:03:30 +0100

    charset setting

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="icon" type="image/x-icon" href="/logo.svg"></link>
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
